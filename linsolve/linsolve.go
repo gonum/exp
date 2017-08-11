6b74fea2acddb80a834fa51e480ecbc7c1240667
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2017-08-11 23:54:45 +0200

    linsolve: update documentation according to PR comments

--- linsolve/linsolve.go
+++ linsolve/linsolve.go
@@ -5,132 +5,123 @@
 // Package linsolve provides iterative algorithms for solving linear systems.
 package linsolve
 
-// Method is an iterative method that produces a sequence of vectors converging
-// to the vector x satisfying a system of linear equations
+// TODO(vladimir-ch): Improve documentation. Write an introduction about
+// iterative methods and that they can be more efficient than direct methods
+// when we are solving large (sparse) systems, when the solution does not have
+// to be known to machine precision. Write that the matrix is accessed only via
+// matrix-vector products. Write that the documentation is written from the
+// perspective of users who want to call Iterative and designers who want to
+// implement Method (or direct users of Method?).
+
+// Method is an iterative method that produces a sequence of vectors that
+// converge to the solution of the system of linear equations
 //  A x = b,
 // where A is non-singular dim×dim matrix, and x and b are vectors of dimension
 // dim.
 //
 // Method uses a reverse-communication interface between the iterative algorithm
 // and the caller. Method acts as a client that commands the caller to perform
-// needed operations via Operation returned from the Iterate method. This
+// needed operations via an Operation returned from the Iterate method. This
 // provides independence of Method on representation of the matrix A, and
 // enables automation of common operations like checking for convergence and
 // maintaining statistics.
 type Method interface {
-	// Init initializes the method for solving
-	// a dim×dim linear system.
+	// Init initializes the method for solving a dim×dim
+	// linear system.
 	Init(dim int)
 
-	// Iterate retrieves data from Context,
-	// updates it, and returns the next
-	// operation. The caller must perform the
-	// Operation using data in Context, and
-	// depending on the state call Iterate
-	// again.
+	// Iterate retrieves data from Context, updates it,
+	// and returns the next operation. The caller must
+	// perform the Operation using data in Context, and
+	// depending on the state call Iterate again.
 	Iterate(*Context) (Operation, error)
 }
 
-// Context mediates the communication between the Method and the caller. It must
-// not be modified or accessed apart from the commanded Operations.
+// Context mediates the communication between the Method and
+// the caller. The caller must not modify Context apart from
+// the commanded Operations.
 type Context struct {
-	// X is the current approximate solution.
-	// On the first call to Method.Iterate, X
-	// must contain the initial estimate.
-	// Method must update X with the current
-	// estimate when it commands
-	// ComputeResidual and EndIteration.
+	// X is the current approximate solution. On the
+	// first call to Method.Iterate, X must contain the
+	// initial estimate. Method will update X with the
+	// current estimate when it commands ComputeResidual
+	// and EndIteration.
 	X []float64
-	// Residual is the current residual b-A*x.
-	// On the first call to Method.Iterate,
-	// Residual must contain the initial
-	// residual.
-	// TODO(vladimir-ch): Consider whether the
-	// behavior should also include: Method
-	// must update Residual with the current
-	// value of b-A*x when it commands
-	// EndIteration.
+	// Residual is the current residual b-A*x. On the
+	// first call to Method.Iterate, Residual must
+	// contain the initial residual.
+	// TODO(vladimir-ch): Consider whether the behavior
+	// should also include: Method will update Residual
+	// with the current value of b-A*x when it commands
+	// EndIteration. Probably not because of GMRES.
 	Residual []float64
-	// ResidualNorm is (an estimate of) the
-	// norm of the current residual. Method
-	// must update it when it commands
-	// CheckConvergence. It is not necessarily
-	// equal to the norm of Residual, some
-	// methods (e.g., GMRES) can estimate the
-	// residual norm without forming the
-	// residual itself.
-	// TODO(vladimir-ch): Actually the exact
-	// behavior here  is something that should
-	// be discussed.
+	// ResidualNorm is (an estimate of) the norm of the
+	// current residual. Method will update it when it
+	// commands CheckConvergence. It is not necessarily
+	// equal to the norm of Residual, some methods
+	// (e.g., GMRES) can estimate the residual norm
+	// without forming the residual itself.
+	// TODO(vladimir-ch): Actually the exact behavior
+	// here  is something that should be discussed.
 	ResidualNorm float64
 	// Converged indicates to Method that the
-	// ResidualNorm satisfies the stopping
-	// criterion as a result of
-	// CheckConvergence operation. If a Method
-	// commands EndIteration with Converged
-	// true, the caller must not call
-	// Method.Iterate again without calling
-	// Method.Init first.
+	// ResidualNorm satisfies the stopping criterion as
+	// a result of CheckConvergence operation. If a
+	// Method commands EndIteration with Converged true,
+	// the caller must not call Method.Iterate again
+	// without calling Method.Init first.
 	Converged bool
 
-	// Src and Dst are the source and
-	// destination vectors for various
-	// Operations.
+	// Src and Dst are the source and destination
+	// vectors for various Operations. See the Operation
+	// documentation for more information.
 	Src, Dst []float64
+	// Trans indicates whether MulVec and PreconSolve
+	// operations must be performed with a matrix
+	// transpose.
+	Trans bool
 }
 
 // Operation specifies the type of operation.
-type Operation uint64
+type Operation uint
 
 // Operations commanded by Method.Iterate.
 const (
 	NoOperation Operation = 0
 
-	// Multiply A*x where x is stored in
-	// Context.Src. The result will be
-	// stored in Context.Dst.
-	MatVec Operation = 1 << (iota - 1)
+	// Compute
+	//  A*x    if Context.Trans==false, or
+	//  A^T*x  if Context.Trans==true,
+	// where x is stored in Context.Src.
+	// The result must be placed in Context.Dst.
+	MulVec Operation = 1 << (iota - 1)
 
-	// Multiply A^T*x where x is stored in
-	// Context.Src. The result will be
-	// stored in Context.Dst.
-	MatTransVec
+	// Perform a preconditioner solve
+	//  M z = r    if Context.Trans==false, or
+	//  M^T z = r, if Context.Trans==true,
+	// where r is stored in Context.Src. The solution z
+	// must be placed in Context.Dst.
+	PreconSolve
 
-	// Do the preconditioner solve
-	//  M z = r,
-	// where r is stored in Context.Src, and
-	// store the solution z in Context.Dst.
-	PSolve
-
-	// Do the preconditioner solve
-	//  M^T z = r,
-	// where r is stored in Context.Src, and
-	// store the solution z in Context.Dst.
-	PSolveTrans
-
-	// Compute b - A*x where x is stored in
-	// Context.X and store the result into
-	// Context.Residual.
+	// Compute b-A*x where x is stored in Context.X. The
+	// result must be placed into Context.Residual.
 	ComputeResidual
 
-	// Check convergence using the current
-	// approximation in Context.X and the
-	// residual norm in Context.ResidualNorm.
-	// Context.Converged must be set to
-	// indicate whether convergence has been
-	// determined, and then Method.Iterate
-	// must be called again.
+	// Check convergence using the current approximation
+	// in Context.X and the residual norm in
+	// Context.ResidualNorm. The caller must set
+	// Context.Converged to indicate whether convergence
+	// has been determined, and then it must call
+	// Method.Iterate again.
 	CheckConvergence
 
-	// EndIteration indicates that Method has
-	// finished what it considers to be one
-	// iteration. It can be used to update an
-	// iteration counter. If Context.Converged
-	// is true, Context.X contains the
-	// approximate solution and the iterative
-	// process must be terminated. Method.Init
-	// must be called before calling
-	// Method.Iterate again.
+	// EndIteration indicates that Method has finished
+	// what it considers to be one iteration. If
+	// Context.Converged is true, Context.X contains the
+	// approximate solution and the caller must
+	// terminate the iterative process. If the caller
+	// performs a new iterative run, it must call
+	// Method.Init before calling Method.Iterate.
 	EndIteration
 )
 
