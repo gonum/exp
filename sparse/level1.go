aa2b571f5659e1be76ad3ca0bb19a8dd02608334
Rémi Louf <remi@vimies.com>
2018-01-09 07:20:22 +0100

    Add implementation of level 1 Sparse BLAS

---
+++ sparse/level1.go
@@ -0,0 +1,204 @@
+// Copyright 2018 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by BSD-style
+// license that can be found in the LICENSE file.
+//
+// Functions in this file implement the Level 1 BLAS specifications
+// for sparse vectors
+//
+// Dot: dot product between sparse vectors
+// Nrm2: norm 2 of the vector
+// Asum: compute the sum of amplitudes of the vector' elements
+// Idmax: returns the index of the element with maximum absolute value
+// Swap: swap two vectors x <-> y
+// Scal: product of a vector by a scalar a*x
+// Axpy: compute z = a*x + y
+// Copy: copy an array y <- x
+// Scatter: y|x <- x
+// Gather: x <- y|x and y|x = 0
+
+package blas64
+
+import "math"
+
+// SparseVector represents a sparse vector.
+type SparseVector struct {
+	Size int // Number of elements in the (non sparse) vector.
+	Ind  []int
+	Data []float64
+}
+
+type DenseVector struct {
+	Data []float64
+}
+
+// Level 1 --------------------------------------------------------------------
+
+//  Dot computes the dot product between two sparse vectors
+//  Dot(x, y) = \sum_i x[i]*y[i]
+//  Will panic if vectors are not the same size
+func Dot(x, y SparseVector) float64 {
+	if x.Size != y.Size {
+		panic("X and Y must have the same length.")
+	}
+	var dot float64
+	ity := 0
+	j := y.Ind[ity]
+	for _, i := range x.Ind {
+		for j <= i {
+			ity += 1
+			j = y.Ind[ity]
+		}
+		if i == j {
+			dot += y.Data[i] * y.Data[j]
+		}
+	}
+	return dot
+}
+
+// Nrm2 computes the L2 norm of the vector x:
+// Nrm2(x) = \sum_i x[i]*x[i]
+func Nrm2(x SparseVector) float64 {
+	var nrm2 float64
+	for _, value := range x.Data {
+		nrm2 += value * value
+	}
+	return math.Sqrt(nrm2)
+}
+
+// Asum computes the sum of the absolute values of the elemnts of x:
+// Asum(x) = \sum_i |x[i]|
+func Asum(x SparseVector) float64 {
+	var asum float64
+	for _, value := range x.Data {
+		asum += math.Abs(value)
+	}
+	return asum
+}
+
+// Idmax returns the index of an element of x with the largest aboslute value
+// If there are multiple such indices the maximum is returned.
+// Idmax returns -1 if n == 0
+func Idmax(x SparseVector) int {
+	if x.Size == 0 {
+		return 0
+	}
+	imax := x.Ind[0]
+	max := x.Data[0]
+	for i, ind := range x.Ind {
+		if x.Data[i] > max {
+			imax = ind
+			max = x.Data[i]
+		}
+	}
+	return imax
+}
+
+// Swap exchanges the elements of the two vectors x and y
+// x[i], y[i] = y[i], x[i] for all i
+// Will panic if the vectors are not of the same length
+func Swap(x, y SparseVector) {
+	ity := 0
+	j := y.Ind[ity]
+	for itx, i := range x.Ind {
+		for j <= i {
+			copy(y.Ind[ity:], y.Ind[ity+1:])
+			y.Ind[len(y.Ind)-1] = 0 // zero value
+			y.Ind = y.Ind[:len(y.Ind)-1]
+			copy(y.Data[ity:], y.Data[ity+1:])
+			y.Data[len(y.Data)-1] = 0 // zero value
+			y.Data = y.Data[:len(y.Data)-1]
+			ity += 1
+		}
+		if i == j { // if index present in both, swap values
+			x.Data[i], y.Data[i] = y.Data[i], x.Data[i]
+		} else {
+			copy(x.Ind[itx:], x.Ind[itx+1:])
+			x.Ind[len(x.Ind)-1] = 0 // zero value
+			x.Ind = x.Ind[:len(x.Ind)-1]
+			copy(y.Data[itx:], x.Data[itx+1:])
+			x.Data[len(y.Data)-1] = 0 // zero value
+			x.Data = x.Data[:len(x.Data)-1]
+			ity += 1
+		}
+	}
+}
+
+// Scal scales the vector x by \alpha:
+// x[i] *= \alpha for all i
+func Scal(alpha float64, x SparseVector) SparseVector {
+	if alpha == 0 {
+		return SparseVector{x.Size, []int{}, []float64{}}
+	}
+
+	for i, _ := range x.Data {
+		x.Data[i] *= alpha
+	}
+	return x
+}
+
+// Copy copies the elements of x into the elements of y
+// y[i] = x[i] for all i
+// Will panic if x and y are not of same length
+func Copy(x, y SparseVector) {
+	if x.Size != y.Size {
+		panic("X and Y must have the same length.")
+	}
+	y.Ind = y.Ind[:0]
+	y.Data = y.Data[:0]
+	for i, ind := range x.Ind {
+		y.Ind = append(y.Ind, ind)
+		y.Data = append(y.Data, x.Data[i])
+	}
+}
+
+// Axpy add alpha time x to y
+// y[i] += alpha * x[i] for all i
+// Current version does not keep an ordered vec
+func Axpy(x, y SparseVector, alpha float64) {
+	ity := 0
+	j := y.Ind[ity]
+	for itx, i := range x.Ind {
+		for j < i {
+			y.Ind = append(y.Ind, i)
+			y.Data = append(y.Data, alpha*x.Data[itx])
+			ity += 1
+			j = y.Ind[ity]
+		}
+		if i == j {
+			y.Data[j] += alpha * x.Data[i]
+			ity += 1
+			j = y.Ind[ity]
+		}
+	}
+}
+
+// Gather takes a DenseVector as an input and
+// transforms it in a sparse vector
+func Gather(x DenseVector) SparseVector {
+	var y SparseVector
+	for i, datum := range x.Data {
+		if datum != 0 {
+			y.Ind = append(y.Ind, i)
+			y.Data = append(x.Data, datum)
+		}
+	}
+	return y
+}
+
+// Scatter builds a DenseVector from a SparseVector
+func Scatter(x DenseVector, y SparseVector) {
+	ity := 0
+	j := y.Ind[ity]
+	for i := 0; i < y.Size; i++ {
+		if i == j {
+			x.Data = append(x.Data, y.Data[j])
+			ity += 1
+			j = y.Ind[ity]
+		} else {
+			x.Data = append(x.Data, 0)
+			ity += 1
+			j = y.Ind[ity]
+		}
+
+	}
+}
---
+++ sparse/level1_test.go
@@ -0,0 +1,400 @@
+package blas64
+
+import (
+	"testing"
+)
+
+// Single vector operations ---------------------------------------------------
+type OneVectorCase struct {
+	Name      string
+	Vector    SparseVector
+	Panic     bool
+	Asum      float64
+	Nrm2      float64
+	Idmax     int
+	ScalCases []ScalCase
+}
+
+type ScalCase struct {
+	Alpha  float64
+	Answer SparseVector // Values of the sparse vector
+	Name   string
+}
+
+var OneVectorCases = []OneVectorCase{
+	{
+		Name:   "Dense All Positive",
+		Vector: SparseVector{5, []int{0, 1, 2, 3, 4}, []float64{6, 5, 4, 2, 6}},
+		Panic:  false,
+		Asum:   23,
+		Nrm2:   10.81665382639196787935766380241148783875388972153573863813135,
+		Idmax:  0,
+		ScalCases: []ScalCase{
+			{
+				Alpha:  0,
+				Answer: SparseVector{5, []int{}, []float64{}},
+			},
+			{
+				Alpha:  1,
+				Answer: SparseVector{5, []int{0, 1, 2, 3, 4}, []float64{6, 5, 4, 2, 6}},
+			},
+			{
+				Alpha:  -2,
+				Answer: SparseVector{5, []int{0, 1, 2, 3, 4}, []float64{-12, -10, -8, -4, -12}},
+			},
+		},
+	},
+	{
+		Name:   "Sparse All Positive",
+		Vector: SparseVector{15, []int{1, 3, 4, 10, 14}, []float64{6, 5, 4, 2, 6}},
+		Panic:  false,
+		Asum:   23,
+		Nrm2:   10.81665382639196787935766380241148783875388972153573863813135,
+		Idmax:  1,
+		ScalCases: []ScalCase{
+			{
+				Alpha:  0,
+				Answer: SparseVector{15, []int{}, []float64{}},
+			},
+			{
+				Alpha:  1,
+				Answer: SparseVector{15, []int{1, 3, 4, 10, 14}, []float64{6, 5, 4, 2, 6}},
+			},
+			{
+				Alpha:  -2,
+				Answer: SparseVector{15, []int{1, 3, 4, 10, 14}, []float64{-12, -10, -8, -4, -12}},
+			},
+		},
+	},
+	{
+		Name:   "Dense All negative",
+		Vector: SparseVector{5, []int{0, 1, 2, 3, 4}, []float64{-6, -5, -4, -2, -6}},
+		Panic:  false,
+		Asum:   23,
+		Nrm2:   10.81665382639196787935766380241148783875388972153573863813135,
+		Idmax:  3,
+		ScalCases: []ScalCase{
+			{
+				Alpha:  0,
+				Answer: SparseVector{5, []int{}, []float64{}},
+			},
+			{
+				Alpha:  1,
+				Answer: SparseVector{5, []int{0, 1, 2, 3, 4}, []float64{-6, -5, -4, -2, -6}},
+			},
+			{
+				Alpha:  -2,
+				Answer: SparseVector{5, []int{0, 1, 2, 3, 4}, []float64{12, 10, 8, 4, 12}},
+			},
+		},
+	},
+	{
+		Name:   "Sparse All Negative",
+		Vector: SparseVector{15, []int{1, 3, 4, 10, 14}, []float64{-6, -5, -4, -2, -6}},
+		Panic:  false,
+		Asum:   23,
+		Nrm2:   10.81665382639196787935766380241148783875388972153573863813135,
+		Idmax:  10,
+		ScalCases: []ScalCase{
+			{
+				Alpha:  0,
+				Answer: SparseVector{15, []int{}, []float64{}},
+			},
+			{
+				Alpha:  1,
+				Answer: SparseVector{15, []int{1, 3, 4, 10, 14}, []float64{-6, -5, -4, -2, -6}},
+			},
+			{
+				Alpha:  -2,
+				Answer: SparseVector{15, []int{1, 3, 4, 10, 14}, []float64{12, 10, 8, 4, 12}},
+			},
+		},
+	},
+	{
+		Name:   "Empty",
+		Vector: SparseVector{0, []int{}, []float64{}},
+		Panic:  false,
+		Asum:   0,
+		Nrm2:   0.0,
+		Idmax:  0,
+		ScalCases: []ScalCase{
+			{
+				Alpha:  0,
+				Answer: SparseVector{0, []int{}, []float64{}},
+			},
+			{
+				Alpha:  1,
+				Answer: SparseVector{0, []int{}, []float64{}},
+			},
+			{
+				Alpha:  -2,
+				Answer: SparseVector{0, []int{}, []float64{}},
+			},
+		},
+	},
+}
+
+// Two vectors operations -----------------------------------------------------
+type TwoSparseVectorsCase struct {
+	Name      string
+	Vector1   SparseVector
+	Vector2   SparseVector
+	Panic     bool
+	Dot       float64
+	CopyCases []CopyCase
+	ScalCases []ScalCase
+}
+
+type CopyCase struct {
+	Name  string
+	Input SparseVector
+	Panic bool
+}
+
+var TwoSparseVectorsCases = []TwoSparseVectorsCase{
+	{
+		Name:    "Dense All Positive",
+		Vector1: SparseVector{5, []int{0, 1, 2, 3, 4}, []float64{6, 5, 4, 2, 6}},
+		Vector2: SparseVector{5, []int{0, 1, 2, 3, 4}, []float64{6, 5, 4, 2, 6}},
+		Panic:   false,
+		Dot:     117,
+		ScalCases: []ScalCase{
+			{
+				Name:   "Zero alpha",
+				Alpha:  0,
+				Answer: SparseVector{5, []int{0, 1, 2, 3, 4}, []float64{6, 5, 4, 2, 6}},
+			},
+			{
+				Name:   "Positive alpha",
+				Alpha:  1,
+				Answer: SparseVector{5, []int{0, 1, 2, 3, 4}, []float64{12, 10, 8, 4, 12}},
+			},
+			{
+				Name:   "Negative alpha",
+				Alpha:  -2,
+				Answer: SparseVector{5, []int{0, 1, 2, 3, 4}, []float64{-6, -5, -4, -2, -6}},
+			},
+		},
+		CopyCases: []CopyCase{
+			{
+				Name:  "Input does not have the same length",
+				Panic: true,
+				Input: SparseVector{1, []int{0}, []float64{0}},
+			},
+			{
+				Name:  "Same length but Ind and Data not matching",
+				Panic: false,
+				Input: SparseVector{5, []int{0, 1, 1}, []float64{0}},
+			},
+			{
+				Name:  "Size, Ind and Data Matching",
+				Panic: false,
+				Input: SparseVector{5, []int{0, 1, 1, 1, 1}, []float64{0, 3, 4, 5, 1}},
+			},
+		},
+	},
+}
+
+// Mixed vectors operations ---------------------------------------------------
+
+type MixedVectorsCase struct {
+	Name   string
+	Sparse SparseVector
+	Dense  DenseVector
+	Panic  bool
+}
+
+var MixedVectorsCases = []MixedVectorsCase{
+	{
+		Name:   "Dense to dense",
+		Sparse: SparseVector{5, []int{0, 1, 2, 3, 4}, []float64{0, 4, 6, 3, 0}},
+		Dense:  DenseVector{[]float64{0, 4, 6, 3, 0}},
+		Panic:  false,
+	},
+}
+
+// Test single vector operations ----------------------------------------------
+
+func TestNrm2(t *testing.T) {
+	for _, c := range OneVectorCases {
+		n := Nrm2(c.Vector)
+		if n != c.Nrm2 {
+			t.Errorf("nrm2: mismatch %v: expected %v, found %v", c.Name, c.Nrm2, n)
+		}
+	}
+}
+
+func TestAsum(t *testing.T) {
+	for _, c := range OneVectorCases {
+		n := Asum(c.Vector)
+		if n != c.Asum {
+			t.Errorf("asum: mismatch %v: expected %v, found %v", c.Name, c.Asum, n)
+		}
+	}
+}
+
+func TestIdmax(t *testing.T) {
+	for _, c := range OneVectorCases {
+		n := Idmax(c.Vector)
+		if n != c.Idmax {
+			t.Errorf("idmax: mismatch %v: expected %v, found %v", c.Name, c.Idmax, n)
+		}
+	}
+}
+
+func TestAscal(t *testing.T) {
+	for _, c := range OneVectorCases {
+		for _, scal := range c.ScalCases {
+			scaled := Scal(scal.Alpha, c.Vector)
+			// Size
+			if len(scaled.Ind) != len(scaled.Data) {
+				t.Errorf("ascal: mismatch of data and indices list size in %v: data %v, indices %v", c.Name, len(scaled.Data), len(scaled.Ind))
+			}
+			if scaled.Size != scal.Answer.Size {
+				t.Errorf("ascal: mismatch of size in %v: expected %v, found %v", c.Name, scal.Answer.Size, scaled.Size)
+			}
+			if len(scaled.Ind) != len(scal.Answer.Ind) {
+				t.Errorf("ascal: mismatch of indices list size in %v: expected %v, found %v", c.Name, len(scal.Answer.Ind), len(scaled.Ind))
+			}
+			// Indices (sanity check)
+			if scal.Answer.Size != 0 {
+				for i, ind := range scaled.Ind {
+					if ind != scal.Answer.Ind[i] {
+						t.Errorf("ascal: mismatch of indices in %v: expected %v, found %v", c.Name, ind, scal.Answer.Ind[i])
+					}
+				}
+			}
+			// Values
+			if scal.Answer.Size != 0 {
+				for i, val := range scaled.Data {
+					if val != scal.Answer.Data[i] {
+						t.Errorf("ascal: mismatch of data in %v: expected %v, found %v", c.Name, scal.Answer.Data[i], val)
+					}
+				}
+			}
+		}
+	}
+}
+
+// Test 2 sparse vectors cases ------------------------------------------------
+
+func TestDot(t *testing.T) {
+	for _, c := range TwoSparseVectorsCases {
+		dotProduct := Dot(c.Vector1, c.Vector2)
+		dotProductsym := Dot(c.Vector2, c.Vector1)
+		if dotProduct != dotProductsym {
+			t.Errorf("dot: the result is not symmetrical in the inputs in %v: %d and %d", c.Name, dotProduct, dotProductsym)
+		}
+		if dotProduct != c.Dot {
+			t.Error("dot: mismatch of dot product value in %v: expected %v, got %v", c.Name, c.Dot, dotProduct)
+		}
+
+	}
+}
+
+func TestSwap(t *testing.T) {
+	for _, c := range TwoSparseVectorsCases {
+		Vec1Copy := SparseVector{Size: c.Vector1.Size, Ind: c.Vector1.Ind, Data: c.Vector1.Data}
+		Vec2Copy := SparseVector{Size: c.Vector2.Size, Ind: c.Vector2.Ind, Data: c.Vector2.Data}
+		Swap(&c.Vector1, &c.Vector2)
+		if !isEqualSparse(c.Vector2, Vec1Copy) {
+			t.Error("swap: the vectors are different in %v", c.Name)
+		}
+		if !isEqualSparse(c.Vector1, Vec2Copy) {
+			t.Error("swap: the vectors are different in %v", c.Name)
+		}
+	}
+}
+
+func TestCopy(t *testing.T) {
+	for _, c := range TwoSparseVectorsCases {
+		for _, cop := range c.CopyCases {
+			// Check if funcion panics
+			func() {
+				if cop.Panic { // If we expect the function to panic
+					defer func() {
+						if r := recover(); r == nil {
+							t.Errorf("copy: %v in %v should have panicked", cop.Name, c.Name)
+						}
+					}()
+					Copy(&c.Vector1, &cop.Input)
+				} else {
+					Copy(&c.Vector1, &cop.Input)
+					if !isEqualSparse(cop.Input, c.Vector1) {
+						t.Error("copy: %v: %v: expected vectors to be equal", c.Name, cop.Name)
+					}
+				}
+			}()
+		}
+	}
+}
+
+func TestAxpy(t *testing.T) {
+	for _, c := range TwoSparseVectorsCases {
+		for _, cal := range c.ScalCases {
+			// Create a deep copy of y
+			var output = SparseVector{Size: c.Vector2.Size,
+				Ind:  make([]int, len(c.Vector2.Ind)),
+				Data: make([]float64, len(c.Vector2.Data))}
+			copy(output.Ind, c.Vector2.Ind)
+			copy(output.Data, c.Vector2.Data)
+			// Test
+			Axpy(&c.Vector1, &output, cal.Alpha)
+			if !isEqualSparse(output, cal.Answer) {
+				t.Errorf("axpy: %s: %s: output does not match the expected vector", c.Name, cal.Name)
+			}
+		}
+	}
+}
+
+// Test 1 sparse and 1 dense vectors cases ------------------------------------
+
+func testFromDense(t *testing.T) {
+	for _, c := range MixedVectorsCases {
+		output := Gather(c.Dense)
+		if !isEqualSparse(output, c.Sparse) {
+			t.Errorf("fromDense: %s: output does not match the expected vector", c.Name)
+		}
+	}
+}
+
+func testScatter(t *testing.T) {
+	for _, c := range MixedVectorsCases {
+		output := Scatter(c.Sparse)
+		if !isEqualDense(output, c.Dense) {
+		}
+	}
+}
+
+// Helper functions -----------------------------------------------------------
+
+func isEqualSparse(x, y SparseVector) bool {
+	if x.Size != y.Size {
+		return false
+	}
+	for idx, i := range x.Ind {
+		if i != y.Ind[idx] {
+			return false
+		}
+	}
+	for idx, d := range x.Data {
+		if d != y.Data[idx] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isEqualDense(x, y DenseVector) bool {
+	if len(x.Data) != len(y.Data) {
+		return false
+	}
+	for i, dx := range x.Data {
+		if dx != y.Data[i] {
+			return false
+		}
+	}
+
+	return true
+}
