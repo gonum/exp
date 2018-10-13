357675eeae77d77cc4af729a0a34a1d5b397a147
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2018-10-13 08:39:41 +0200

    linsolve: add BCGSTAB method

---
+++ linsolve/bcgstab.go
@@ -0,0 +1,144 @@
+// Copyright ©2017 The gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package linsolve
+
+import (
+	"errors"
+	"math"
+
+	"gonum.org/v1/gonum/floats"
+)
+
+// BCGSTAB implements the BiConjugate Gradient Stabilized method with
+// preconditioning for solving systems of linear equations
+//  A*x = b,
+// where A is a square, generally nonsymmetric matrix.
+//
+//
+// References:
+//  - Barrett, R. et al. (1994). Section 2.3.8 BiConjugate Gradient Stabilized (Bi-CGSTAB).
+//    In Templates for the Solution of Linear Systems: Building Blocks
+//    for Iterative Methods (2nd ed.) (pp. 24-25). Philadelphia, PA: SIAM.
+//    Retrieved from http://www.netlib.org/templates/templates.pdf
+type BCGSTAB struct {
+	first  bool
+	resume int
+
+	rho, rhoPrev float64
+	alpha        float64
+	omega        float64
+
+	rt   []float64
+	v    []float64
+	t    []float64
+	p    []float64
+	phat []float64
+	shat []float64
+}
+
+// Init implements the Method interface.
+func (b *BCGSTAB) Init(dim int) {
+	if dim <= 0 {
+		panic("bcgstab: dimension not positive")
+	}
+
+	b.rt = reuse(b.rt, dim)
+	b.p = reuse(b.p, dim)
+	b.v = reuse(b.v, dim)
+	b.t = reuse(b.t, dim)
+	b.phat = reuse(b.phat, dim)
+	b.shat = reuse(b.shat, dim)
+
+	b.first = true
+	b.resume = 1
+}
+
+// Iterate implements the Method interface. It will command the following
+// operations:
+//  MulVec
+//  PreconSolve
+//  CheckResidual
+//  MajorIteration
+//  NoOperation
+func (b *BCGSTAB) Iterate(ctx *Context) (Operation, error) {
+	switch b.resume {
+	case 1:
+		if b.first {
+			copy(b.rt, ctx.Residual)
+		}
+		b.rho = floats.Dot(b.rt, ctx.Residual)
+		if math.Abs(b.rho) < rhoBreakdownTol {
+			b.resume = 0
+			return NoOperation, errors.New("bcgstab: rho breakdown")
+		}
+		if b.first {
+			b.first = false
+			copy(b.p, ctx.Residual)
+		} else {
+			beta := (b.rho / b.rhoPrev) * (b.alpha / b.omega)
+			floats.AddScaled(b.p, -b.omega, b.v)
+			floats.Scale(beta, b.p)
+			floats.Add(b.p, ctx.Residual)
+		}
+		// Solve M^{-1} * p_i.
+		copy(ctx.Src, b.p)
+		b.resume = 2
+		return PreconSolve, nil
+	case 2:
+		copy(b.phat, ctx.Dst)
+		// Compute A * p^_i.
+		copy(ctx.Src, b.phat)
+		b.resume = 3
+		return MulVec, nil
+	case 3:
+		copy(b.v, ctx.Dst)
+		rtv := floats.Dot(b.rt, b.v)
+		if rtv == 0 {
+			b.resume = 0
+			return NoOperation, errors.New("bcgstab: breakdown")
+		}
+		b.alpha = b.rho / rtv
+		// Form the residual and X so that we can check for tolerance early.
+		floats.AddScaled(ctx.X, b.alpha, b.phat)
+		floats.AddScaled(ctx.Residual, -b.alpha, b.v)
+		b.resume = 4
+		return CheckResidual, nil
+	case 4:
+		if ctx.Converged {
+			b.resume = 5
+			return MajorIteration, nil
+		}
+		fallthrough
+	case 5:
+		// Solve M^{-1} * r_i.
+		copy(ctx.Src, ctx.Residual)
+		b.resume = 6
+		return PreconSolve, nil
+	case 6:
+		copy(b.shat, ctx.Dst)
+		// Compute A * s^_i.
+		copy(ctx.Src, b.shat)
+		b.resume = 7
+		return MulVec, nil
+	case 7:
+		copy(b.t, ctx.Dst)
+		b.omega = floats.Dot(b.t, ctx.Residual) / floats.Dot(b.t, b.t)
+		floats.AddScaled(ctx.X, b.omega, b.shat)
+		floats.AddScaled(ctx.Residual, -b.omega, b.t)
+		b.resume = 8
+		return CheckResidual, nil
+	case 8:
+		if !ctx.Converged && math.Abs(b.omega) < omegaBreakdownTol {
+			b.resume = 0
+			return NoOperation, errors.New("bcgstab: omega breakdown")
+		}
+		b.rhoPrev = b.rho
+		b.resume = 1
+		return MajorIteration, nil
+
+	default:
+		panic("bcgstab: Init not called")
+	}
+}
