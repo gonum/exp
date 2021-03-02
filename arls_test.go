adf91a19cd8915589e46dfa50be58c35d1d808e0
rejones7go <77038517+rejones7go@users.noreply.github.com>
2021-03-01 20:54:57 -0700

    Update arls_test.go
    Updating license to use gonum's, not go's

--- arls_test.go
+++ arls_test.go
@@ -1,4 +1,7 @@
 package arls
+// Copyright ©2021 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 import "fmt"
 import "math"
 import "testing"
