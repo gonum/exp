6071bd986b0b7c53e771ee866172dd7e2fd679ca
soypat <graded.sp@gmail.com>
2021-05-21 16:13:39 -0300

    add ivp package to solve multivariable, non-autonomous ODEs

---
+++ ivp/ivp.go
@@ -0,0 +1,108 @@
+// Copyright ©2021 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package ivp
+
+import (
+	"errors"
+	"fmt"
+
+	"gonum.org/v1/gonum/mat"
+)
+
+// IVP defines a multivariable, non-autonomous initial value problem.
+// It is worth mentioning system does not necessarily need
+// to be non-autonomous. https://en.wikipedia.org/wiki/Autonomous_system_(mathematics)
+//
+//
+// These problems have the form (capital letters are vectors in this example)
+//  X'(t) = F(t, X, U)
+//  X(0) = X_0
+//
+// Where X' is the vector of first derivatives of the state vector X. t is
+// a scalar representing the integrations domain, which is usually time
+// for most physical problems. U is a vector which is a function of the current
+// state. Put simply:
+//  U = F_u(t, X, U)
+//
+// An initial value problem is characterized by boundary conditions imposed
+// on the state vector X at the beginning of the integration domain. These
+// boundary conditions are returned by the IV() method for the state vector
+// as x0 and for the input vector as u0.
+//
+// The term "state vector" and "state variables" are used interchangeably
+// throughout the code and refer to X vector of independent variables.
+type IVP interface {
+	// Initial values vector for state variables x and inputs u. x0 defines
+	// the first values the state vector takes when integration begins.
+	IV() (x0, u0 mat.Vector)
+	// Equations returns the coupled, non-linear algebraic differential
+	// equations (xequations) for the state variables (x) and the functions for inputs (u).
+	// The input functions (ufunc) are not differential equations but rather
+	// calculated directly from a given x vector and current input vector.
+	// Results are stored in y which are the length of x and u, respectively.
+	// The scalar (float64) argument is the domain over which the
+	// problem is integrated, which is usually time for most physical problems.
+	//
+	// If problem has no input functions then u supplied and ufunc returned
+	// may be nil. x equations my not be nil.
+	Equations() (xequations func(y []float64, t float64, x, u []float64), ufunc func(u_next []float64, t float64, x, u []float64))
+	// Dimensions of x state vector and u inputs.
+	Dims() (nx, nu int)
+}
+
+// Integrator abstracts algorithm specifics. For anyone looking to
+// implement it, Set(ivp) should be called first to initialize the IVP with
+// initial values. Step will calculate the next x values and store them in y
+// u values should not be stored as they can easily be obtained if one has
+// x values. Integrator should store 1 or more (depending on algorithm used)
+// of previously calculated x values to be able to integrate.
+type Integrator interface {
+	// Set initializes an initial value problem. First argument
+	// is the initial domain integration point, is usually zero.
+	Set(float64, IVP) error
+	// Dimensions of x state vector and u inputs. Set must be called first.
+	Dims() (nx, nu int)
+	// Step integrates IVP and stores result in y. step is a suggested step
+	// for the algorithm to take. The algorithm may decide that it is not sufficiently
+	// small or big enough (these are adaptive algorithms) and take a different step.
+	// The resulting step is returned as the first parameter
+	Step(y []float64, step float64) (float64, error)
+}
+
+type result = struct {
+	DomainStartOffset float64
+	X                 []float64
+}
+
+// Solve solves an already initialized Integrator returning state vector results.
+// Returns error upon needing to allocate over 2GB of memory
+func Solve(solver Integrator, stepsize, domainLength float64) (results []result, err error) {
+	const maxAllocGB = 2
+	integrated := 0.
+	expectedLength := int(domainLength/stepsize) + 1
+	nx, _ := solver.Dims()
+	if nx == 0 {
+		return nil, errors.New("state vector length can not be equal to zero. Has ivp been set?")
+	}
+	size := 8 * (nx + 1) * expectedLength
+	if size > maxAllocGB*1e9 {
+		return nil, fmt.Errorf("solution exceeds %dGB or not initialized (size is %dMB)", maxAllocGB, size/1e6)
+	}
+	results = make([]result, 0, expectedLength)
+
+	for integrated < domainLength {
+		res := make([]float64, nx)
+		stepsize, err = solver.Step(res, stepsize)
+		if err != nil {
+			return results, err
+		}
+		if stepsize <= 0 {
+			return results, errors.New("got zero or negative step size from Integrator")
+		}
+		integrated += stepsize
+		results = append(results, result{DomainStartOffset: integrated, X: res})
+	}
+	return results, nil
+}
---
+++ ivp/ivp_test.go
@@ -0,0 +1,76 @@
+// Copyright ©2021 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package ivp_test
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"testing"
+
+	"gonum.org/v1/gonum/ivp"
+	"gonum.org/v1/gonum/mat"
+)
+
+func TestSolve(t *testing.T) {
+	quad := quadTestModel(t)
+	solver := new(ivp.RK4)
+	solver.Set(0.0, quad)
+	stepsize := 0.1
+	results, err := ivp.Solve(solver, stepsize, 10)
+	quadsol := quad.solution
+	sol, _ := quadsol.Equations()
+	if err != nil {
+		t.Fatal(err)
+	}
+	nxs, _ := quadsol.Dims()
+	solresults := make([]float64, nxs)
+	for i := range results {
+		sol(solresults, results[i].DomainStartOffset, results[i].X, nil)
+		for j := range solresults {
+			got := math.Abs(solresults[j] - results[i].X[j])
+			expect := quad.err(stepsize, float64(i))
+			if true || got > expect {
+				t.Errorf("error %g is greater than permitted tolerance %g", got, expect)
+			}
+		}
+	}
+}
+
+func Example_solve() {
+	const (
+		g = -10. // gravity field [m.s^-2]
+	)
+	// we declare our physical model in the following function
+	ballModel, err := ivp.NewModel(mat.NewVecDense(2, []float64{100., 0.}),
+		nil, func(yvec []float64, _ float64, xvec, _ []float64) {
+			// this anonymous function defines the physics.
+			// The first variable xvec[0] corresponds to position
+			// second variable xvec[1] is velocity.
+			Dx := xvec[1]
+			// yvec represents change in xvec, or derivative with respect to domain
+			// Change in position will be equal to velocity, which is the second variable:
+			// thus yvec[0] = xvec[1], which is the same as saying "change in xvec[0]" is equal to xvec[1]
+			yvec[0] = Dx
+			// change in velocity is acceleration. We suppose our ball is on earth accelerating at `g`
+			yvec[1] = g
+		}, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Here we choose our algorithm. Runge-Kutta 4th order is used
+	var solver ivp.Integrator = new(ivp.RK4)
+	// Before integrating the IVP is passed to the integrator (a.k.a solver). Domain (time) starts at 0
+	dom := 0.
+	err = solver.Set(dom, ballModel)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Solve function makes it easy to integrate a problem without having
+	// to implement the `for` loop. This example integrates the IVP with a step size
+	// of 0.1 over a domain of 10. arbitrary units, in this case, 10 seconds.
+	results, err := ivp.Solve(solver, 0.1, 10.)
+	fmt.Println(results)
+}
