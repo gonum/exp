f9f51aa16ee02e63b1e9ab44f864ea37e8371185
Patricio Whittingslow <graded.sp@gmail.com>
2022-04-30 00:23:08 -0300

    ode: add Runge-Kutta-Nyström 12(10) method

--- ode/config.go
+++ ode/config.go
@@ -1,3 +1,7 @@
+// Copyright ©2022 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package ode
 
 // Parameters represents the most common configuration parameters
